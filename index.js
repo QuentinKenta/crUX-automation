39c2236860b0234c4d5d0a2f4718fc7b5df05f22
Quentin MATHIEU <quentin.mathieu.ext@louisvuitton.com>
2023-06-01 00:12:23 +0200

    adding api url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function callCruXAPI() {
   try {
-    const response = await axios.get('https://api.example.com/crux');
+    const response = await axios.get('https://chromeuxreport.googleapis.com/v1/records:queryRecord?key=AIzaSyAf0EHL9xP4tQwHNAIYTBWeDZBB_Ij2gys');
     console.log(response.data);
     return response.data;
   } catch (error) {
