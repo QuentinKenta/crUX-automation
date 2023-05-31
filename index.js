3ebd1599730c31977bd3933de85629365d837ac9
Quentin MATHIEU <quentin.mathieu.ext@louisvuitton.com>
2023-05-31 23:39:38 +0200

    Transformaing main.yml into calling crux aPI from index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-    // 'who-to-greet' input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date().toTimeString());
-    core.setOutput("time",time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2 )
-    //console.log(`The event payload: ${payload}`);
-} catch (error)
-{
-    core.setFailed(error.message);
+async function callCruXAPI() {
+  try {
+    const response = await axios.get('https://api.example.com/crux');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error calling CruX API:', error.message);
+    throw error;
+  }
 }
+
+callCruXAPI()
+  .then((response) => {
+    console.log('API response:', response);
+    process.stdout.write(`::set-output name=response::${response}`);
+  })
+  .catch((error) => {
+    process.exitCode = 1;
+  });
